05b9a61808bb6469cbf8293d39744dc09e9bb561
Steve Smith <steve@kentsmiths.com>
2023-11-13 19:37:52 -0500

    Adding unit tests

---
+++ module6/demos/after/MegaPricer.UnitTests/PricingServiceDecorator_CalculatePrice.cs
@@ -0,0 +1,88 @@
+﻿using MegaPricer.Models;
+using FluentAssertions;
+using MegaPricer.Interfaces;
+using NSubstitute;
+using MegaPricer.Services;
+using MegaPricer.Data;
+using Ardalis.Result;
+
+namespace MegaPricer.UnitTests;
+public class PricingServiceDecorator_CalculatePrice
+{
+  private readonly string _testUserName = "test";
+
+  public PricingServiceDecorator_CalculatePrice()
+  {
+    Context.Session.Clear();
+    Context.Session.Add(_testUserName, new Dictionary<string, object>());
+  }
+
+  [Fact]
+  public void ReturnsZeroGivenPricingOff()
+  {
+    var pricingService = GetPricingService();
+    var decorator = new PricingServiceDecorator(pricingService);
+    var priceRequest = GetValidPriceRequest();
+    var priceCalculationStrategy = Substitute.For<IPriceCalculationStrategy>();
+    Context.Session[_testUserName]["PricingOff"] = "Y";
+
+    var result = decorator.CalculatePrice(priceRequest, priceCalculationStrategy);
+
+    result.Value.Should().BeEquivalentTo(new PriceGroup(0, 0, 0));
+  }
+
+  [Fact]
+  public void ReturnsInvalidGivenWallOrderNumZero()
+  {
+    var pricingService = GetPricingService();
+    var decorator = new PricingServiceDecorator(pricingService);
+    var priceRequest = GetValidPriceRequest();
+    priceRequest.wallOrderNum = 0;
+    var priceCalculationStrategy = Substitute.For<IPriceCalculationStrategy>();
+    Context.Session[_testUserName]["PricingOff"] = "N";
+
+    var result = decorator.CalculatePrice(priceRequest, priceCalculationStrategy);
+
+    result.Status.Should().Be(ResultStatus.Forbidden);
+  }
+
+  [Fact]
+  public void ReturnsInvalidGivenKitchenIdZero()
+  {
+    var pricingService = GetPricingService();
+    var decorator = new PricingServiceDecorator(pricingService);
+    var priceRequest = GetValidPriceRequest();
+    priceRequest.kitchenId = 0;
+    var priceCalculationStrategy = Substitute.For<IPriceCalculationStrategy>();
+    Context.Session[_testUserName]["PricingOff"] = "N";
+
+    var result = decorator.CalculatePrice(priceRequest, priceCalculationStrategy);
+
+    result.Status.Should().Be(ResultStatus.Invalid);
+    result.ValidationErrors.First().Should()
+      .BeEquivalentTo(new ValidationError("invalid kitchenId"));
+  }
+
+  private PriceRequest GetValidPriceRequest()
+  {
+    return new PriceRequest()
+    {
+      userName = _testUserName,
+      wallOrderNum = 1,
+      kitchenId = 1
+    };
+  }
+
+  private PricingService GetPricingService()
+  {
+    return new PricingService(
+        Substitute.For<IGetUserMarkup>(),
+        Substitute.For<IKitchenDataService>(),
+        Substitute.For<IWallDataService>(),
+        Substitute.For<ICabinetDataService>(),
+        Substitute.For<IPartCostDataService>(),
+        Substitute.For<IFeatureDataService>(),
+        Substitute.For<IPartPricingService>()
+        );
+  }
+}
---
+++ module6/demos/after/MegaPricer.UnitTests/PricingService_CalculatePrice.cs
@@ -0,0 +1,49 @@
+﻿using MegaPricer.Models;
+using FluentAssertions;
+using MegaPricer.Interfaces;
+using NSubstitute;
+using MegaPricer.Services;
+using Ardalis.Result;
+
+namespace MegaPricer.UnitTests;
+
+public class PricingService_CalculatePrice
+{
+  private readonly string _testUserName = "test";
+
+  [Fact]
+  public void ReturnsInvalidGivenInvalidWallResult()
+  {
+    var wallDataService = Substitute.For<IWallDataService>();
+    wallDataService.GetWall(Arg.Any<int>(), Arg.Any<int>())
+      .Returns(Result.Invalid(new ValidationError("invalid wall")));
+
+    var pricingService = new PricingService(
+        Substitute.For<IGetUserMarkup>(),
+        Substitute.For<IKitchenDataService>(),
+        wallDataService,
+        Substitute.For<ICabinetDataService>(),
+        Substitute.For<IPartCostDataService>(),
+        Substitute.For<IFeatureDataService>(),
+        Substitute.For<IPartPricingService>()
+        );
+    var priceRequest = GetValidPriceRequest();
+    var priceCalculationStrategy = Substitute.For<IPriceCalculationStrategy>();
+
+    var result = pricingService.CalculatePrice(priceRequest, priceCalculationStrategy);
+
+    result.Status.Should().Be(ResultStatus.Invalid);
+    result.ValidationErrors.First().Should().BeEquivalentTo(new ValidationError("invalid wall"));
+  }
+
+  private PriceRequest GetValidPriceRequest()
+  {
+    return new PriceRequest()
+    {
+      userName = _testUserName,
+      wallOrderNum = 1,
+      kitchenId = 1
+    };
+  }
+
+}
